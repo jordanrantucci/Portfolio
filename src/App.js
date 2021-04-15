db8835b4b053cff7b191831702dee9da3b2b5c5f
Jordan Rantucci <jordanrantucci@gmail.com>
2021-04-14 22:48:52 -0400

    added a logo and navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
+    <div>
+    <Navbar />
    <h2>Portfolio Project</h2>
+   </div>
   );
 }
 
