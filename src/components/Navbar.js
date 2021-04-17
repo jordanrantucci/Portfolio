2cfff004ee8ed41fbfed6853bd200c0d62ab6dac
Jordan Rantucci <jordanrantucci@gmail.com>
2021-04-17 00:06:09 -0400

    added a font awesome hamburger menu white to appear on a dark background

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import logo from '../assets/JRC_logo.png'
+import logo from '../assets/JRC_logo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
     return (
@@ -7,7 +9,7 @@ const Navbar = () => {
         <div className="container"> 
             <a classNameName="navbar-brand" href="#"><img className="logo" src={logo} alt="logo"></img></a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
+                <FontAwesomeIcon icon={faBars} style={{ color: "#fff"}} />
             </button>
 
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
