3efab8daf50591a1772dae3ee5a408fff5954b0b
Jordan Rantucci <jordanrantucci@gmail.com>
2021-04-16 23:36:38 -0400

    adding styling and future links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,9 @@ import logo from '../assets/JRC_logo.png'
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a classNameName="navbar-brand" href="#"><img src={logo}></img></a>
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+        <div className="container"> 
+            <a classNameName="navbar-brand" href="#"><img className="logo" src={logo} alt="logo"></img></a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
@@ -15,11 +16,18 @@ const Navbar = () => {
                         <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Link</a>
+                        <a className="nav-link" href="#">About Me</a>
+                    </li>
+                      <li className="nav-item">
+                        <a className="nav-link" href="#">Portfolio</a>
+                    </li>
+                      <li className="nav-item">
+                        <a className="nav-link" href="#">Contact</a>
                     </li>
                 </ul>
                     
-  </div>
+            </div>
+        </div>
 </nav>
     )
 }
