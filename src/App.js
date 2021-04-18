df37360208ffd6cbef022ae90ecd0201a0e83099
Jordan Rantucci <jordanrantucci@gmail.com>
2021-04-18 00:37:34 -0400

    adding particle movement

--- src/App.js
+++ src/App.js
@@ -2,10 +2,36 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
+import Particles from 'react-particles-js';
 
 function App() {
   return (
     <>
+      <Particles
+        params={{
+          particles: {
+            line_linked: {
+              shadow: {
+                enable: true,
+                color: "#FF8C00",
+                blur: 1,
+                opacity: 1,
+                width: 100,
+                distance: 1
+              }
+            },
+            shape: {
+              type: "square",
+              stroke: {
+                width:5,
+                color: "#1E90FF"
+              }
+            },
+            move: {
+              speed: 1,
+            }
+          }
+        }} />
     <Navbar />
     <Header />
    </>
