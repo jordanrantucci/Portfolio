ef1ed295c0ca48f1c624decc1a7fce082ef79619
Jordan Rantucci <jordanrantucci@gmail.com>
2021-04-20 23:49:30 -0400

    fixed more routing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light  bg-dark">
         <div className="container"> 
-            <a className="navbar-brand" href="/"><img className="logo" src={logo} alt="logo"></img></a>
+            <a className="navbar-brand" href="#/"><img className="logo" src={logo} alt="logo"></img></a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <FontAwesomeIcon icon={faBars} style={{ color: "#fff"}} />
             </button>
@@ -15,7 +15,7 @@ const Navbar = () => {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav ml-auto">
                     <li className="nav-item active">
-                        <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+                        <a className="nav-link" href="#/">Home <span className="sr-only">(current)</span></a>
                     </li>
                     <li className="nav-item">
                         <a className="nav-link" href="#/AboutMe_React">About Me</a>
